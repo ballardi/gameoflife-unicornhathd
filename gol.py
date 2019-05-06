6f99fa3b1360cce3540e50879067c67efbe65fb3
ballardi <1449350+ballardi@users.noreply.github.com>
2019-05-06 16:55:56 +0900

    use a Node class, and clean up script inputs
    Make the board an array of Node objects.
    Move script params to top.
    Stop using numpy.

--- gol.py
+++ gol.py
@@ -2,37 +2,50 @@
 
 import unicornhathd
 import time, colorsys
-import numpy
 
+STARTING_VALS = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]  
+
+SLEEP_BETWEEN_FRAMES = 0.5
 
 #############################
+class Node:
+    on = 0
+    
+#############################
 def generateInitialBoard():
-    board = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]  
 
-    board = numpy.array(board)
+    # create board with zeros
+    w, h = 16, 16;
+    board = [[Node() for x in range(w)] for y in range(h)]
+    
+    # set starting values for board
+    for y in range(16):
+        for x in range(16):
+            board[x][y].on = STARTING_VALS[x][y]
+    
     return board
     
 #############################
 def toggleFirstPixel(board):
-        if board[0,0] == 0:
-            board[0,0] = 1
+        if board[0][0].on == 0:
+            board[0][0].on = 1
         else:
-            board[0,0] = 0
+            board[0][0].on = 0
 
 #############################
 def updateGameWorld(board):
@@ -43,7 +56,7 @@ def updateGameWorld(board):
 def setPixelForCoord(x, y):
     h = 0.4 # blue
     s = 1.0 # saturation
-    v = board[x,y]*0.1 # brightness
+    v = board[x][y].on*.1 # brightness
     
     # convert the hsv back to RGB
     rgb = colorsys.hsv_to_rgb(h, s, v) 
@@ -78,7 +91,7 @@ try:
                 
         unicornhathd.show() # show the pixels
         
-        time.sleep(1) # waiting time between heartbeats
+        time.sleep(SLEEP_BETWEEN_FRAMES) # waiting time between heartbeats
 
 except KeyboardInterrupt:
     unicornhathd.off()
